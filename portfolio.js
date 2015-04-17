51672347f7bd1f1a1ba440c20b8d2535324ead40
mrluker <luke.m.riley@gmail.com>
2015-04-17 18:37:35 -0500

    Scripts moved from bottom of index.html to new portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+
+    // custom transformation: scale header's title
+    var titleStyle = document.querySelector('.title').style;
+    addEventListener('core-header-transform', function(e) {
+      var d = e.detail;
+      var m = d.height - d.condensedHeight;
+      var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
+      titleStyle.transform = titleStyle.webkitTransform =
+          'scale(' + scale + ') translateZ(0)';
+    });
+
+        function change() {
+      var s = document.querySelector('select');
+      document.querySelector('core-animated-pages').transitions = document.querySelector('select').options[s.selectedIndex].value;
+    }
+
+    function stuff(page) {
+      var p = document.querySelector('core-animated-pages');
+      p.selected = page;
+
+    }
+    function sendMessage(el) {
+    el.show();
+  }
+    
